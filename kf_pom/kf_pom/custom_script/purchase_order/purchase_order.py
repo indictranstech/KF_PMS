93039dcec74a072314f4d4a304b8bf96ebb140ea
Neha0037 <ntalokar@yahoo.com>
2021-08-24 18:38:16 +0530

    add customer,customer address on MR & PO and Custom Print Format

--- kf_pom/kf_pom/custom_script/purchase_order/purchase_order.py
+++ kf_pom/kf_pom/custom_script/purchase_order/purchase_order.py
@@ -4,7 +4,7 @@ import frappe
 import json
 
 def validate(doc,method=None):
-	
+	# Make Approvers comments on PO madatory for certain approvers
 	if((doc.workflow_state == 'Rejected by Director Approver' )and 'Director Approver' in frappe.get_roles()) :
 		if doc.	approver_comments == None or doc.approver_comments == '':
 			frappe.throw("Approver's comments are mandatory")
@@ -21,3 +21,12 @@ def validate(doc,method=None):
 		if doc.approver_comments:
 			doc.add_comment('Comment', text=doc.approver_comments)
 			doc.approver_comments = ''
+
+	# #Send mails to the user and approvers
+	# #creating url for doctype to send in email
+ #    url = frappe.utils.get_url()
+ #    doctype_url = url + "/desk#Form/Purchase Order/"+doc.name
+ #    team_email=frappe.db.get_value("Team",{'name':doc.team},"team_email")
+
+ #    msg ="""Dear %s Team,<br><br>Purchase Order %s <br>Following are the details. <br>"""%(doc.team,doc.name)
+ #    frappe.sendmail(recipients=team_email,subject="Purchase Order Status",content=msg)
