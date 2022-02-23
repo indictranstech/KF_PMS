6db9a2b638ae84212e3cfefe93bf8c746ccb26c7
jitendra.r <jitendra.r@indictranstech.com>
2022-02-23 18:50:16 +0530

    Fix-Remove unwanted space

--- kf_pom/kf_pom/custom_script/address/address.py
+++ kf_pom/kf_pom/custom_script/address/address.py
@@ -16,7 +16,6 @@ def user_query(doctype,txt,searchfield, start, page_len, filters):
 					and name like %(txt)s""".format(key=searchfield), {
         			'txt': "%{}%".format(txt)})
 
-
 def get_permission_query_conditions(doctype):
 	if "Requestor/Site Manager" in frappe.get_roles():
 		user = frappe.session.user
